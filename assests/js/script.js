5b10f500cd7e659073e3db6f69bbc72c4cc77c55
unknown <jtjackson325@gmail.com>
2021-10-09 15:20:36 -0600

    got more of the questions typed out

--- assests/js/script.js
+++ assests/js/script.js
@@ -19,6 +19,48 @@ function selectedAnswer() {
 
 const question = [
     {
-        question: ''
+        question: 'What is the getElementByIdMethod used for',
+        answers: [
+            {text: "To change the content of an html element", correct: false},
+            {text: "selects an html element/container", correct: true},
+            {text: "passes a value into another function", correct: false},
+            {text: "finds error in the text", correct: false}
+        ],
+        question: "What command logs informaiton in the console",
+        answers: [
+            {text: "console.log()", correct: true},
+            {text: "createElement", correct: false},
+            {text: "startWriting()", correct: false},
+            {text: "addClassList()", correct: false}
+        ],
+        question: "How do I add a child element to the parent element",
+        answers: [
+            {text: "appendchild()", correct: false},
+            {text: "makechild()", correct: false},
+            {test: "joinchild()", correct: false},
+            {test: "appendChild()", correct: true}
+        ],
+        question: "What does the method innerHtml do",
+        answers: [
+            {text: "renders html elements on the page", correct: false},
+            {text: "leaves the html text the same", correct:false},
+            {text: "sets or returns the html content of an element", corret: true},
+            {text: "switches places with an html tag", correct: false}
+        ],
+        quesiton: "How do you enclose an array",
+        answers: [
+            {text: "curly brackets", correct: false},
+            {text: "square brackets", correct: true},
+            {text: "forward slashes", correct: false},
+            {test: "backward slashes", correct: false}
+        ],
+        question: "How do we initiate startFunction() as a funciton in javascript",
+        answers: [
+            {text: "simply state the funciton at the top of the file", correct: false},
+            {text: "only type the command startFunciton()", correct: false},
+            {text: "type startFunciton() again at the bottom of the file", correct: true},
+            {text: "only type startFunction() at the top of the file", correct: false}
+        ],
+        question: ""
     }
 ]
